511de7e95ffb5afda2c3f68069d644849db6dc72
Tanzim-hash <atanzim04@gmail.com>
2025-07-15 20:19:18 +0600

    Updated greeting message

--- bin/dart_github_practice.dart
+++ bin/dart_github_practice.dart
@@ -1,5 +1,6 @@
-import 'package:dart_github_practice/dart_github_practice.dart' as dart_github_practice;
+import 'package:dart_github_practice/dart_github_practice.dart'
+    as dart_github_practice;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_github_practice.calculate()}!');
+  print('Hello from GitHub-connected Dart app!');
 }
