3902c294f2fe8893d00601f44a657eaa811a8212
Tanzim-hash <atanzim04@gmail.com>
2025-07-15 20:14:53 +0600

    Initial commit: Dart console project created

---
+++ bin/dart_github_practice.dart
@@ -0,0 +1,5 @@
+import 'package:dart_github_practice/dart_github_practice.dart' as dart_github_practice;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_github_practice.calculate()}!');
+}
---
+++ test/dart_github_practice_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_github_practice/dart_github_practice.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
