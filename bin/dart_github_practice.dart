24ee91c4c6cea2b388fa65d4b883dfb3808195ce
Tanzim-hash <atanzim04@gmail.com>
2025-07-16 11:57:48 +0600

    vscode commit

--- bin/dart_github_practice.dart
+++ bin/dart_github_practice.dart
@@ -1,6 +1,4 @@
-import 'package:dart_github_practice/dart_github_practice.dart'
-    as dart_github_practice;
-
 void main(List<String> arguments) {
   print('Hello from GitHub-connected Dart app!');
+  print("This is from a new branch!");
 }
